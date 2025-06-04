34db49c06be1595695be5980da2f985ab2882eee
rainbowsurfer137 <erica@smiberts.com>
2025-06-04 12:55:54 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const shareIcon = document.querySelector('.icon-wrapper');
+const footer = document.querySelector('.footer');
+const tooltipMenu = document.querySelector('.tooltips-menu');
+
+shareIcon.addEventListener('click', () => {
+//   footer.classList.toggle('active');
+if (window.innerWidth >= 768) {
+    tooltipMenu.classList.toggle('active');
+    footer.classList.remove('active');
+} else {
+    footer.classList.toggle('active');
+    tooltipMenu.classList.remove('active');
+}
+});
